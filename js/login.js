cba817fed7138955f3b1095994f5c157cf7a3385
jão <110256135+jaovit@users.noreply.github.com>
2024-08-01 19:43:13 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -2,6 +2,6 @@
 document.getElementById('g_id_onload').setAttribute('data-client_id', '278083335487-r4hcr6jcnesdaga60bvq2ov9d5hbbhjv.apps.googleusercontent.com');
 
 function loginCredenciais(response) {
-    let dados = jwt_decode(response.credential);
+    let dados = response.credential
     console.log(dados);
   }
