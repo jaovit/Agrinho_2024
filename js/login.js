10be9e4b18bfb9af41af3dbd43d68b3b41518a8d
jão <110256135+jaovit@users.noreply.github.com>
2024-08-01 19:21:06 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 // 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
 
 function loginCredenciais(response) {
-    let dados = response.json
-    console.log("Encoded JWT ID token: " + response.credential);
+    let dados = jwt_decode(response.credential);
+    console.log(dados);
 }
